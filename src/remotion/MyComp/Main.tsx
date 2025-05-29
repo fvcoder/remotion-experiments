ef02f098d5d875ff9c0f21b36ba98596de78f96a
Fernando Ticona <thefersh24@gmail.com>
2025-05-29 12:05:43 -0400

    verdades incomodas

--- src/remotion/MyComp/Main.tsx
+++ src/remotion/MyComp/Main.tsx
@@ -4,11 +4,13 @@ import {
 import React from "react";
 import { ProgressBarIntro } from "./scene/progressBarIntro";
 import { SpeakerIntro } from "./scene/speakerIntro";
+import { UncomfortableTruths } from "./scene/uncomfortableTruths";
 
 export const Main = () => {
 
   return (
     <AbsoluteFill className="bg-white">
+      <UncomfortableTruths />
       <SpeakerIntro />
       <ProgressBarIntro />
     </AbsoluteFill>
---
+++ src/remotion/MyComp/scene/uncomfortableTruths.tsx
@@ -0,0 +1,45 @@
+import { AbsoluteFill, Easing, Img, interpolate, Sequence, staticFile, useCurrentFrame, useVideoConfig } from "remotion";
+
+function Images() {
+    const frame = useCurrentFrame();
+    const { width } = useVideoConfig();
+    const opacity = interpolate(frame, [0, 10], [0, 1], {
+        easing: Easing.bezier(0.33, 1, 0.68, 1),
+        extrapolateLeft: "clamp",
+        extrapolateRight: "clamp",
+    })
+    const staticWidth = (width - (40 * 4)) / 3;
+    const w1 = interpolate(frame, [20, 40, 100, 120, 260, 280], [staticWidth, 591, 591, 200, 200, staticWidth], {
+        easing: Easing.bezier(0.33, 1, 0.68, 1),
+        extrapolateLeft: "clamp",
+        extrapolateRight: "clamp",
+    })
+    const w2 = interpolate(frame, [20, 40, 100, 120, 180, 200, 260, 280], [staticWidth, 200, 200, 591, 591, 200, 200, staticWidth], {
+        easing: Easing.bezier(0.33, 1, 0.68, 1),
+        extrapolateLeft: "clamp",
+        extrapolateRight: "clamp",
+    })
+    const w3 = interpolate(frame, [20, 30, 180, 200, 260, 280], [staticWidth, 200, 200, 591, 591, staticWidth], {
+        easing: Easing.bezier(0.33, 1, 0.68, 1),
+        extrapolateLeft: "clamp",
+        extrapolateRight: "clamp",
+    })
+
+    return (
+        <AbsoluteFill className="p-10 justify-center">
+            <div className="flex items-center gap-10 h-4/5" style={{ opacity }}>
+                <Img src={staticFile("f.jpg")} className="h-full object-cover rounded-4xl block flex-none" style={{ width: w1 }} />
+                <Img src={staticFile("s.jpg")} className="h-full object-cover rounded-4xl block flex-none" style={{ width: w2 }} />
+                <Img src={staticFile("n.jpg")} className="h-full object-cover rounded-4xl block flex-none" style={{ width: w3 }} />
+            </div>
+        </AbsoluteFill>
+    )
+}
+
+export function UncomfortableTruths() {
+    return (
+        <Sequence from={245} durationInFrames={350} name="UncomfortableTruths">
+            <Images />
+        </Sequence>
+    )
+}
